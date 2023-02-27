d40389256683253c328b2dea9bd9643d48a2204e
KirKirKsd <sapetp54@gmail.com>
2023-02-27 16:49:29 +0300

    Player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    // movement
+    public Joystick joystick;
+    public float speed = 4f;
+
+    // components
+    public Rigidbody2D rb;
+
+    private void Update() {
+        Movement();
+    }
+
+    void Movement() {
+        Vector2 movement = joystick.Direction;
+        rb.velocity = movement * speed;
+    }
+}
