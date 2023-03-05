c543411c80847724495f3f1cbad1561f33e77fd1
KirKirKsd <sapetp54@gmail.com>
2023-03-05 21:44:48 +0300

    fix balance

--- Assets/Scripts/Variables.cs
+++ Assets/Scripts/Variables.cs
@@ -6,9 +6,9 @@ public class Variables : MonoBehaviour {
     public static float screen_height = Camera.main.ScreenToWorldPoint(new Vector2(Screen.height, 0f)).x * 2;
 
     // basic sizes
-    public static Vector3 I3 = Vector3.one * screen_width / 3;
-    public static Vector3 I2 = Vector3.one * screen_width / 4.5f;
-    public static Vector3 I1 = Vector3.one * screen_width / 6;
+    public static Vector3 I3 = Vector3.one * screen_width / 2;
+    public static Vector3 I2 = Vector3.one * screen_width / 3;
+    public static Vector3 I1 = Vector3.one * screen_width / 4;
 
     // player
     public static Vector3 playerSize = Vector3.one * screen_width / 24;
